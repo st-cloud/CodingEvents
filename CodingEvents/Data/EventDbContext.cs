f654101bdff93eaf421d8797c6b261807a564f2c
Grace Brown <gracenbrown@sbcglobal.net>
2021-01-02 16:04:58 -0600

    Chapter 19 Object-Relational Mapping Reading complete

---
+++ CodingEvents/Data/EventDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using CodingEvents.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodingEvents.Data
+{
+    public class EventDbContext : DbContext
+    {
+        public DbSet<Event> Events { get; set; }
+
+        public EventDbContext(DbContextOptions<EventDbContext> options) : base(options)
+        {
+        }
+    }
+}
--- CodingEvents/Models/Event.cs
+++ CodingEvents/Models/Event.cs
@@ -9,22 +9,17 @@ namespace CodingEvents.Models
 
         public EventType Type { get; set; }
 
-        public int Id { get; }
-        private static int nextId = 1;
+        public int Id { get; set; }
 
         public Event()
         {
-            Id = nextId;
-            nextId++;
         }
 
-        public Event(string name, string description, string contactEmail): this()
+        public Event(string name, string description, string contactEmail)
         {
             Name = name;
             Description = description;
-            Id = nextId;
             ContactEmail = contactEmail;
-            nextId++;
         }
 
         public override string ToString()
